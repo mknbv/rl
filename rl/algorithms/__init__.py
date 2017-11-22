170b4bfafd1f9f2e10fff517ad911983347fc73b
Michael <konobeev.michael@gmail.com>
2017-11-22 14:54:08 +0300

    Improve package structure

--- rl/algorithms/__init__.py
+++ rl/algorithms/__init__.py
@@ -1,2 +1,3 @@
 from rl.algorithms.base import *
 from rl.algorithms.a3c import *
+import rl.algorithms.wrappers
--- train_spec.py
+++ train_spec.py
@@ -1,14 +1,14 @@
 from gym.envs.atari import AtariEnv
 import tensorflow as tf
 
-from rl.policies import *
+import rl
 
 def create_optimizer(env, policy, global_step):
   optimizer_class = tf.train.AdamOptimizer
-  if isinstance(policy, SimplePolicy):
+  if isinstance(policy, rl.policies.SimplePolicy):
     learning_rate = 1e-3
-  elif isinstance(policy, UniverseStarterPolicy):
+  elif isinstance(policy, rl.policies.UniverseStarterPolicy):
     learning_rate = 1e-4
-  elif isinstance(policy, CNNPolicy):
+  elif isinstance(policy, rl.policies.CNNPolicy):
     raise NotImplementedError()
   return optimizer_class(learning_rate=learning_rate)
