74c177db87f1e0fd0e09cfba12999e76d1c6834c
Michael <konobeev.michael@gmail.com>
2018-05-29 12:29:00 +0300

    Add env_batch

--- rl/__init__.py
+++ rl/__init__.py
@@ -3,5 +3,6 @@ import rl.policies
 import rl.data
 import rl.trainers
 
-from rl.utils import tf_utils
+from rl.utils import env_batch
 from rl.utils import env_wrappers
+from rl.utils import tf_utils
