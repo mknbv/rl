3cd6fe607be7d2e7d94d76659da63b306d7216d4
Michael <konobeev.michael@gmail.com>
2018-06-02 14:35:00 +0300

    GAE assumes env batch

--- rl/algorithms/advantages.py
+++ rl/algorithms/advantages.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-__all__ = ["GAE"]
-
 
 class GAE(object):
   def __init__(self, policy, gamma=0.99, lambda_=0.95, normalize=False):
@@ -11,22 +9,32 @@ class GAE(object):
     self._normalize = normalize
 
   def __call__(self, trajectory, sess=None):
-    num_timesteps = trajectory["num_timesteps"]
-    gae = np.zeros([num_timesteps])
-    gae[-1] = trajectory["rewards"][num_timesteps-1]\
-        - trajectory["critic_values"][num_timesteps-1]
-    if not trajectory["resets"][num_timesteps-1]:
-      obs = trajectory["latest_observation"]
-      gae[-1] += self._gamma * self._policy.act(obs, sess)[1]
-    for i in reversed(range(num_timesteps-1)):
-      not_reset = 1 - trajectory["resets"][i] # i is for next state
+    gae = np.zeros(trajectory["env_steps"], dtype=np.float32)
+    num_envs = trajectory["latest_observations"].shape[0]
+    gae[-num_envs:] = (
+        trajectory["rewards"][-num_envs:]
+        - trajectory["critic_values"][-num_envs:,0]
+    )
+    obs = trajectory["latest_observations"]
+    last_values = sess.run(self._policy.critic_tensor,
+                           {self._policy.observations: obs})[:,0]
+    gae[-num_envs:] += (
+        (1 - trajectory["resets"][-num_envs:]) * self._gamma * last_values)
+
+    for i in range(gae.shape[0] - num_envs, 0, -num_envs):
+      not_reset = 1 - trajectory["resets"][i-num_envs:i]
+      next_critic_values = trajectory["critic_values"][i:i+num_envs,0]
       delta = (
-          trajectory["rewards"][i]
-          + not_reset * self._gamma * trajectory["critic_values"][i+1]
-          - trajectory["critic_values"][i]
+          trajectory["rewards"][i-num_envs:i]
+          + not_reset * self._gamma * next_critic_values
+          - trajectory["critic_values"][i-num_envs:i,0]
       )
-      gae[i] = delta + not_reset * self._gamma * self._lambda_ * gae[i+1]
-    value_targets = gae + trajectory["critic_values"][:num_timesteps]
+      gae[i-num_envs:i] = (
+          delta
+          + not_reset * self._gamma * self._lambda_ * gae[i:i+num_envs]
+      )
+    value_targets = gae[:,None] + trajectory["critic_values"]
+
     if self._normalize:
       gae = (gae - gae.mean()) / (gae.std() + np.finfo(gae.dtype).eps)
     return gae, value_targets
